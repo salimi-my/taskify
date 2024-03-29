fdfae507355ff3d6714330bcdf83a909463bacc0
Mohamad Salimi <contact@salimi.my>
2024-03-30 00:13:50 +0800

    Update auth layout

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -1,7 +1,25 @@
+import Image from 'next/image';
+import BgImage from '@public/auth-layout-bg.jpg';
+
 export default function AuthLayout({
   children
 }: {
   children: React.ReactNode;
 }) {
-  return <>{children}</>;
+  return (
+    <div className='w-full lg:grid min-h-screen lg:grid-cols-3'>
+      <div className='hidden bg-muted lg:block'>
+        <Image
+          src={BgImage}
+          placeholder='blur'
+          priority
+          alt='Auth Image'
+          className='h-full w-full object-cover brightness-90 dark:brightness-[0.2] dark:grayscale'
+        />
+      </div>
+      <div className='flex items-center justify-center py-12 col-span-2'>
+        <div className='mx-auto grid w-[350px] gap-6'>{children}</div>
+      </div>
+    </div>
+  );
 }

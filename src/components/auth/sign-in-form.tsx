1bc5f840c6c79c4f61f71750d99a7e77de8c0446
Mohamad Salimi <contact@salimi.my>
2024-03-30 23:43:50 +0800

    Create sign in form

---
+++ src/components/auth/sign-in-form.tsx
@@ -0,0 +1,119 @@
+'use client';
+
+import * as z from 'zod';
+import Link from 'next/link';
+import { useState } from 'react';
+import { Loader2 } from 'lucide-react';
+import { useForm } from 'react-hook-form';
+import { useSearchParams } from 'next/navigation';
+import { zodResolver } from '@hookform/resolvers/zod';
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+
+const formSchema = z.object({
+  email: z.string().email({ message: 'Please enter valid email address.' }),
+  password: z.string().min(1, { message: 'Please enter password.' })
+});
+
+export function SignInForm() {
+  const searchParams = useSearchParams();
+  const callbackUrl = searchParams.get('callbackUrl');
+  const [loading, setLoading] = useState(false);
+
+  let error = searchParams.get('error');
+
+  if (error === 'CredentialsSignin') {
+    error = 'Invalid email or password.';
+  }
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: '',
+      password: ''
+    }
+  });
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {};
+
+  return (
+    <>
+      <div className='grid gap-2 text-start'>
+        <h1 className='text-3xl font-bold'>Sign in</h1>
+        <p className='text-balance text-muted-foreground'>
+          to continue to your account
+        </p>
+      </div>
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className='flex flex-col gap-4 -mt-1'
+        >
+          <FormField
+            control={form.control}
+            name='email'
+            render={({ field }) => (
+              <FormItem className='space-y-1'>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder='email@example.com'
+                    autoComplete='email'
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name='password'
+            render={({ field }) => (
+              <FormItem className='space-y-1'>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input type='password' placeholder='••••••••' {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <Button
+            disabled={loading}
+            type='submit'
+            variant='default'
+            size='lg'
+            className='mt-4'
+          >
+            {loading && (
+              <>
+                <Loader2 className='animate-spin mr-2' size={18} />
+                Signing in...
+              </>
+            )}
+            {!loading && <>Sign in</>}
+          </Button>
+          <p className='text-sm font-light text-gray-500 dark:text-gray-400 mt-1'>
+            Don&apos;t have an account yet?
+            <Button variant='link' className='ml-1 px-0' asChild>
+              <Link href='/auth/sign-up' title='Sign up'>
+                Sign up
+              </Link>
+            </Button>
+          </p>
+        </form>
+      </Form>
+    </>
+  );
+}

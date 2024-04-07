ebfee51dd9abfad3572ab0ee8318cbb7d657fb73
Mohamad Salimi <contact@salimi.my>
2024-04-08 00:14:00 +0800

    Add credentials provider to auth.config.ts

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -1,7 +1,34 @@
-import GitHub from 'next-auth/providers/github';
-
+import { compare } from 'bcryptjs';
 import type { NextAuthConfig } from 'next-auth';
+import Credentials from 'next-auth/providers/credentials';
+
+import { getUserByEmail } from '@/data/user';
 
 export default {
-  providers: [GitHub]
+  providers: [
+    Credentials({
+      async authorize(credentials) {
+        if (
+          !credentials.email ||
+          !credentials.password ||
+          typeof credentials.email !== 'string' ||
+          typeof credentials.password !== 'string'
+        ) {
+          return null;
+        }
+
+        const user = await getUserByEmail(credentials.email);
+
+        if (
+          !user ||
+          !user.password ||
+          !(await compare(credentials.password, user.password))
+        ) {
+          return null;
+        }
+
+        return user;
+      }
+    })
+  ]
 } satisfies NextAuthConfig;

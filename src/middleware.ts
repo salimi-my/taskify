1168614cce6b96180eb7ae480b350b92ec9fd264
Mohamad Salimi <contact@salimi.my>
2024-04-07 23:59:40 +0800

    Create middleware.ts

---
+++ src/middleware.ts
@@ -0,0 +1,56 @@
+import NextAuth from 'next-auth';
+
+import authConfig from '@/auth.config';
+import {
+  authRoutes,
+  publicRoutes,
+  apiAuthPrefix,
+  DEFAULT_SIGNIN_REDIRECT
+} from '@/routes';
+
+export const { auth } = NextAuth(authConfig);
+
+export default auth((req) => {
+  const { nextUrl } = req;
+  const isSignedIn = !!req.auth;
+
+  const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
+  const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
+  const isAuthRoute = authRoutes.includes(nextUrl.pathname);
+
+  if (isApiAuthRoute || isPublicRoute) {
+    return;
+  }
+
+  if (isAuthRoute) {
+    if (isSignedIn) {
+      return Response.redirect(
+        new URL(DEFAULT_SIGNIN_REDIRECT, nextUrl).toString()
+      );
+    }
+
+    return;
+  }
+
+  if (!isSignedIn && !isPublicRoute) {
+    let callbackUrl = nextUrl.pathname;
+    if (nextUrl.search) {
+      callbackUrl += nextUrl.search;
+    }
+
+    const encodedCallbackUrl = encodeURIComponent(callbackUrl);
+
+    return Response.redirect(
+      new URL(
+        `/auth/sign-in?callbackUrl=${encodedCallbackUrl}`,
+        nextUrl
+      ).toString()
+    );
+  }
+
+  return;
+});
+
+export const config = {
+  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)']
+};

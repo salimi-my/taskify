66c6cd654453b3bddbae850cf25d007f6f825890
Mohamad Salimi <contact@salimi.my>
2024-04-12 01:18:42 +0800

    Create register server action

---
+++ src/actions/register.ts
@@ -0,0 +1,50 @@
+'use server';
+
+import * as z from 'zod';
+import bcrypt from 'bcryptjs';
+
+import { db } from '@/lib/db';
+import { RegisterSchema } from '@/schemas';
+import { getUserByEmail } from '@/data/user';
+
+export async function register(values: z.infer<typeof RegisterSchema>) {
+  const validatedFields = RegisterSchema.safeParse(values);
+
+  if (!validatedFields.success) {
+    return {
+      error: 'Invalid fields.'
+    };
+  }
+
+  const { name, email, password } = validatedFields.data;
+
+  const saltRounds = 10;
+  const salt = await bcrypt.genSalt(saltRounds);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  const existingUser = await getUserByEmail(email);
+
+  if (existingUser) {
+    return {
+      error: 'Email is already in use.'
+    };
+  }
+
+  const newUser = await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword
+    }
+  });
+
+  if (!newUser || !newUser.email) {
+    return {
+      error: 'Uh oh! Something went wrong.'
+    };
+  }
+
+  return {
+    success: 'Sign up successful. Check your email to verify.'
+  };
+}

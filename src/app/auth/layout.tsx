cf6633d13cef6bcd5bc37829ba1b4adfa710e30f
Mohamad Salimi <contact@salimi.my>
2024-04-14 16:27:05 +0800

    Update auth layout

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -1,5 +1,8 @@
 import Image from 'next/image';
-import BgImage from '@public/auth-layout-bg.jpg';
+import Link from 'next/link';
+
+import AuthImage from '@public/auth-image.png';
+import LogoWhite from '@public/taskify-logo-white.png';
 
 export default function AuthLayout({
   children
@@ -8,14 +11,38 @@ export default function AuthLayout({
 }) {
   return (
     <div className='w-full lg:grid min-h-screen lg:grid-cols-3'>
-      <div className='hidden bg-muted lg:block'>
-        <Image
-          src={BgImage}
-          placeholder='blur'
-          priority
-          alt='Auth Image'
-          className='h-full w-full object-cover brightness-90 dark:brightness-[0.2] dark:grayscale'
-        />
+      <div className='relative hidden flex-col justify-between p-10 pb-0 text-white lg:flex bg-primary dark:bg-gray-200 h-full'>
+        <Link href='/' className='flex justify-start items-center'>
+          <Image
+            src={LogoWhite}
+            placeholder='blur'
+            priority
+            alt='Taskify Logo'
+            className='w-7 h-7 mr-3'
+          />
+          <h1 className='flex justify-start items-center font-bold text-2xl'>
+            Taskify
+          </h1>
+        </Link>
+        <div className='w-full max-w-[512px]'>
+          <h2 className='text-5xl font-extrabold'>
+            Organize, Prioritize, Conquer Your Tasks!
+          </h2>
+          <p className='text-muted pt-5 font-light'>
+            Taskify is a comprehensive project management and powerful task
+            management app designed to streamline your workflow, enhance
+            collaboration, and boost productivity.
+          </p>
+        </div>
+        <div className='w-full max-w-[512px]'>
+          <Image
+            src={AuthImage}
+            placeholder='blur'
+            priority
+            alt='Taskify Logo'
+            className='w-full h-full object-cover'
+          />
+        </div>
       </div>
       <div className='flex items-center justify-center py-12 col-span-2 min-h-screen'>
         <div className='mx-auto grid w-[416px] gap-6 px-4'>{children}</div>

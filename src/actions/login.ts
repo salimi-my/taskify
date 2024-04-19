46d65b30cb3699147080765387a877d07a09e058
Mohamad Salimi <contact@salimi.my>
2024-04-20 03:43:21 +0800

    Add 2fa checking

--- src/actions/login.ts
+++ src/actions/login.ts
@@ -3,12 +3,18 @@
 import * as z from 'zod';
 import { AuthError } from 'next-auth';
 
+import { db } from '@/lib/db';
 import { signIn } from '@/auth';
 import { LoginSchema } from '@/schemas';
 import { getUserByEmail } from '@/data/user';
 import { DEFAULT_SIGNIN_REDIRECT } from '@/routes';
-import { sendVerificationEmail } from '@/lib/mail';
-import { generateVerificationToken } from '@/lib/tokens';
+import { getTwoFactorTokenByEmail } from '@/data/two-factor-token';
+import { sendVerificationEmail, sendTwoFactorTokenEmail } from '@/lib/mail';
+import { getTwoFactorConfirmationByUserId } from '@/data/two-factor-confirmation';
+import {
+  generateTwoFactorToken,
+  generateVerificationToken
+} from '@/lib/tokens';
 
 export async function login(
   values: z.infer<typeof LoginSchema>,
@@ -42,6 +48,67 @@ export async function login(
     );
 
     return { success: 'Confirmation email sent. Check your email to verify.' };
+  }
+
+  // Check if 2FA enabled
+  if (existingUser.email && existingUser.isTwoFactorEnabled) {
+    // If verifying 2FA code
+    if (code) {
+      // Verify the 2FA code
+      const twoFactorToken = await getTwoFactorTokenByEmail(existingUser.email);
+
+      if (!twoFactorToken) {
+        return { error: 'Invalid code.' };
+      }
+
+      if (twoFactorToken.token !== code) {
+        return { error: 'Invalid code.' };
+      }
+
+      const hasExpired = new Date(twoFactorToken.expires) < new Date();
+
+      if (hasExpired) {
+        return { error: 'Code has expired.' };
+      }
+
+      // Delete 2FA token
+      await db.twoFactorToken.delete({
+        where: {
+          id: twoFactorToken.id
+        }
+      });
+
+      const existingConfirmation = await getTwoFactorConfirmationByUserId(
+        existingUser.id
+      );
+
+      if (existingConfirmation) {
+        // Delete existing 2FA confirmation
+        await db.twoFactorConfirmation.delete({
+          where: {
+            id: existingConfirmation.id
+          }
+        });
+      }
+
+      await db.twoFactorConfirmation.create({
+        data: {
+          userId: existingUser.id
+        }
+      });
+    }
+    // If not verifying 2FA code
+    else {
+      // Send 2FA code mail
+      const twoFactorToken = await generateTwoFactorToken(existingUser.email);
+      await sendTwoFactorTokenEmail(
+        existingUser.name,
+        twoFactorToken.email,
+        twoFactorToken.token
+      );
+
+      return { twoFactor: true };
+    }
   }
 
   try {

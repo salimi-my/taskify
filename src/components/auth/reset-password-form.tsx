bf8b7c68555d92c5595906d9692bf43c0d7e5c98
Mohamad Salimi <contact@salimi.my>
2024-04-20 00:58:27 +0800

    Create reset password form

---
+++ src/components/auth/reset-password-form.tsx
@@ -0,0 +1,139 @@
+'use client';
+
+import * as z from 'zod';
+import Link from 'next/link';
+import { useForm } from 'react-hook-form';
+import { useState, useTransition } from 'react';
+import { useSearchParams } from 'next/navigation';
+import { ChevronLeft, Loader2 } from 'lucide-react';
+import { zodResolver } from '@hookform/resolvers/zod';
+
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+import { ResetPasswordSchema } from '@/schemas';
+import { FormError } from '@/components/form-error';
+import { FormSuccess } from '@/components/form-success';
+import { resetPassword } from '@/actions/reset-password';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage
+} from '@/components/ui/form';
+
+export function ResetPasswordForm() {
+  const searchParams = useSearchParams();
+  const token = searchParams.get('token');
+
+  const [error, setError] = useState<string | undefined>('');
+  const [success, setSuccess] = useState<string | undefined>('');
+  const [isPending, startTransition] = useTransition();
+
+  const form = useForm<z.infer<typeof ResetPasswordSchema>>({
+    resolver: zodResolver(ResetPasswordSchema),
+    defaultValues: {
+      password: ''
+    }
+  });
+
+  const onSubmit = (values: z.infer<typeof ResetPasswordSchema>) => {
+    setError('');
+    setSuccess('');
+
+    startTransition(() => {
+      resetPassword(values, token).then((data) => {
+        if (data.error) {
+          setError(data.error);
+        }
+
+        if (data.success) {
+          form.reset();
+          setSuccess(data.success);
+        }
+      });
+    });
+  };
+
+  return (
+    <>
+      <div className='grid gap-2 text-start'>
+        <h1 className='text-3xl font-bold'>Reset password</h1>
+        <p className='text-balance text-muted-foreground'>
+          Enter new password for your account
+        </p>
+      </div>
+      <FormError className='mb-2' message={error} />
+      <FormSuccess className='mb-2' message={success} />
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className='flex flex-col gap-4 -mt-1'
+        >
+          <FormField
+            control={form.control}
+            name='password'
+            render={({ field }) => (
+              <FormItem className='space-y-1'>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input
+                    disabled={isPending}
+                    placeholder='••••••••'
+                    type='password'
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name='confirm'
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Confirm Password</FormLabel>
+                <FormControl>
+                  <Input
+                    {...field}
+                    disabled={isPending}
+                    placeholder='••••••••'
+                    type='password'
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <Button
+            disabled={isPending}
+            type='submit'
+            variant='default'
+            size='lg'
+            className='mt-4'
+          >
+            {isPending && (
+              <>
+                <Loader2 className='animate-spin mr-2' size={18} />
+                Processing...
+              </>
+            )}
+            {!isPending && <>Reset password</>}
+          </Button>
+          <div>
+            <Button variant='link' className='-ml-1 px-0' asChild>
+              <Link href='/auth/sign-in' title='Sign up'>
+                <ChevronLeft />
+                Back to Sign in
+              </Link>
+            </Button>
+          </div>
+        </form>
+      </Form>
+    </>
+  );
+}

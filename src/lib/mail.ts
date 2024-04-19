56263ff0bc5b240283c8317589c0d09f2c3f0646
Mohamad Salimi <contact@salimi.my>
2024-04-19 23:38:08 +0800

    Create send password reset email function lib

--- src/lib/mail.ts
+++ src/lib/mail.ts
@@ -1,4 +1,5 @@
 import { Resend } from 'resend';
+import PasswordReset from '@/components/emails/password-reset';
 import EmailVerification from '@/components/emails/email-verification';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
@@ -17,5 +18,20 @@ export async function sendVerificationEmail(
     to: email,
     subject: 'Email Verification',
     react: EmailVerification({ name, baseUrl, verifyLink })
+  });
+}
+
+export async function sendPasswordResetEmail(
+  name: string | null,
+  email: string,
+  token: string
+) {
+  const resetLink = `${baseUrl}/auth/reset-password?token=${token}`;
+
+  await resend.emails.send({
+    from: 'Taskify <reset@taskify.salimi.my>',
+    to: email,
+    subject: 'Password Reset',
+    react: PasswordReset({ name, baseUrl, resetLink })
   });
 }

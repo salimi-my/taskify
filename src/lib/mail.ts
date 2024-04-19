7d259eadfa20aa5fca5c81881c66639788c5a04d
Mohamad Salimi <contact@salimi.my>
2024-04-20 03:27:48 +0800

    Create send two factor token email function lib

--- src/lib/mail.ts
+++ src/lib/mail.ts
@@ -1,10 +1,11 @@
 import { Resend } from 'resend';
+
 import PasswordReset from '@/components/emails/password-reset';
 import EmailVerification from '@/components/emails/email-verification';
-
-const resend = new Resend(process.env.RESEND_API_KEY);
+import TwoFactorAuthentication from '@/components/emails/two-factor-authentication';
 
 const baseUrl = process.env.APP_URL;
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function sendVerificationEmail(
   name: string,
@@ -33,5 +34,18 @@ export async function sendPasswordResetEmail(
     to: email,
     subject: 'Password Reset',
     react: PasswordReset({ name, baseUrl, resetLink })
+  });
+}
+
+export async function sendTwoFactorTokenEmail(
+  name: string | null,
+  email: string,
+  token: string
+) {
+  await resend.emails.send({
+    from: 'Taskify <2fa@taskify.salimi.my>',
+    to: email,
+    subject: 'Two Factor Authentication Code',
+    react: TwoFactorAuthentication({ name, baseUrl, token })
   });
 }

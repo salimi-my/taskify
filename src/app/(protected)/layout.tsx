76f0257b555b3386a1f70ffaf04c9e62794c7a13
Mohamad Salimi <contact@salimi.my>
2024-04-21 15:33:38 +0800

    Update bg color

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -19,7 +19,7 @@ export default function ProtectedLayout({
       <Sidebar />
       <main
         className={cn(
-          'min-h-[calc(100vh_-_56px)] bg-zinc-50 dark:bg-zinc-900 transition-[margin-left] ease-in-out duration-300',
+          'min-h-[calc(100vh_-_56px)] bg-gray-50 dark:bg-gray-900 transition-[margin-left] ease-in-out duration-300',
           sidebar?.isOpen === false ? 'lg:ml-[90px]' : 'lg:ml-72'
         )}
       >

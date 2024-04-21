f68ad180d8b19a1c97ab6168a7c1da2df7090634
Mohamad Salimi <contact@salimi.my>
2024-04-21 14:39:56 +0800

    Create footer component

---
+++ src/components/protected/footer.tsx
@@ -0,0 +1,21 @@
+export function Footer() {
+  return (
+    <div className='supports-backdrop-blur:bg-background/60 z-20 w-full shadow bg-background/95 backdrop-blur'>
+      <div className='mx-8 flex h-14 items-center'>
+        <small className='text-muted-foreground'>
+          © {new Date().getFullYear()} Taskify. Created by{' '}
+          <a
+            className='hover:underline'
+            href='https://www.linkedin.com/in/mohamad-salimi'
+            aria-label='Salimi'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            Salimi
+          </a>
+          . All right reserved.
+        </small>
+      </div>
+    </div>
+  );
+}

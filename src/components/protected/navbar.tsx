f39038fa4234597efb104699d251ab69dc025a4d
Mohamad Salimi <contact@salimi.my>
2024-04-21 14:32:45 +0800

    Create navbar component

---
+++ src/components/protected/navbar.tsx
@@ -0,0 +1,20 @@
+import { UserNav } from '@/components/protected/user-nav';
+import { PageTitle } from '@/components/protected/page-title';
+import { SheetMenu } from '@/components/protected/sheet-menu';
+
+export function Navbar() {
+  return (
+    <header className='z-10 supports-backdrop-blur:bg-background/60 sticky top-0 w-full shadow dark:shadow-secondary bg-background/95 backdrop-blur'>
+      <div className='mx-4 sm:mx-8 flex h-14 items-center'>
+        <div className='flex items-center space-x-4 lg:space-x-0'>
+          <SheetMenu />
+          <PageTitle />
+        </div>
+        <div className='flex flex-1 items-center space-x-2 justify-end'>
+          <p>ModeToggleButton</p>
+          <UserNav />
+        </div>
+      </div>
+    </header>
+  );
+}

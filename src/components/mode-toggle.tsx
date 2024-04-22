f1ccd78ea82425ba20eb59010e0d38f19e3a560f
Mohamad Salimi <contact@salimi.my>
2024-04-22 21:32:01 +0800

    Update mode toggle component

--- src/components/mode-toggle.tsx
+++ src/components/mode-toggle.tsx
@@ -2,7 +2,7 @@
 
 import * as React from 'react';
 import { useTheme } from 'next-themes';
-import { MixerVerticalIcon, MoonIcon, SunIcon } from '@radix-ui/react-icons';
+import { MoonIcon, SunIcon } from '@radix-ui/react-icons';
 
 import { Button } from '@/components/ui/button';
 import {
@@ -11,59 +11,27 @@ import {
   TooltipTrigger,
   TooltipProvider
 } from '@/components/ui/tooltip';
-import {
-  DropdownMenu,
-  DropdownMenuItem,
-  DropdownMenuContent,
-  DropdownMenuTrigger
-} from '@/components/ui/dropdown-menu';
 
 export function ModeToggle() {
-  const { setTheme } = useTheme();
+  const { setTheme, theme } = useTheme();
 
   return (
-    <DropdownMenu>
-      <TooltipProvider disableHoverableContent>
-        <Tooltip delayDuration={100}>
-          <TooltipTrigger asChild>
-            <DropdownMenuTrigger asChild>
-              <Button
-                className='rounded-full w-8 h-8 bg-background'
-                variant='outline'
-                size='icon'
-              >
-                <SunIcon className='w-5 h-5 rotate-0 scale-100 transition-transform ease-in-out duration-500 dark:-rotate-90 dark:scale-0' />
-                <MoonIcon className='absolute w-5 h-5 rotate-90 scale-0 transition-transform ease-in-out duration-500 dark:rotate-0 dark:scale-100' />
-                <span className='sr-only'>Switch Theme</span>
-              </Button>
-            </DropdownMenuTrigger>
-          </TooltipTrigger>
-          <TooltipContent side='bottom'>Switch Theme</TooltipContent>
-        </Tooltip>
-      </TooltipProvider>
-      <DropdownMenuContent align='end'>
-        <DropdownMenuItem
-          className='hover:cursor-pointer'
-          onClick={() => setTheme('light')}
-        >
-          <SunIcon className='h-4 w-4 mr-2 dark:text-foreground' />
-          Light
-        </DropdownMenuItem>
-        <DropdownMenuItem
-          className='hover:cursor-pointer'
-          onClick={() => setTheme('dark')}
-        >
-          <MoonIcon className='h-4 w-4 mr-2 dark:text-foreground' />
-          Dark
-        </DropdownMenuItem>
-        <DropdownMenuItem
-          className='hover:cursor-pointer'
-          onClick={() => setTheme('system')}
-        >
-          <MixerVerticalIcon className='h-4 w-4 mr-2 dark:text-foreground' />
-          System
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <TooltipProvider disableHoverableContent>
+      <Tooltip delayDuration={100}>
+        <TooltipTrigger asChild>
+          <Button
+            className='rounded-full w-8 h-8 bg-background'
+            variant='outline'
+            size='icon'
+            onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
+          >
+            <SunIcon className='w-[1.2rem] h-[1.2rem] rotate-90 scale-0 transition-transform ease-in-out duration-500 dark:rotate-0 dark:scale-100' />
+            <MoonIcon className='absolute w-[1.2rem] h-[1.2rem] rotate-0 scale-1000 transition-transform ease-in-out duration-500 dark:-rotate-90 dark:scale-0' />
+            <span className='sr-only'>Switch Theme</span>
+          </Button>
+        </TooltipTrigger>
+        <TooltipContent side='bottom'>Switch Theme</TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
   );
 }

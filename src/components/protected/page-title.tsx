d6ee67c7d0308d18121b134a048cc603a9fb78be
Mohamad Salimi <contact@salimi.my>
2024-04-25 15:57:55 +0800

    Update page title component

--- src/components/protected/page-title.tsx
+++ src/components/protected/page-title.tsx
@@ -1,17 +1,24 @@
 'use client';
 
+import { getPages } from '@/lib/pages';
 import { usePathname } from 'next/navigation';
 
 export function PageTitle() {
   const pathname = usePathname();
+  const pages = getPages(pathname);
 
-  let pageTitle = '';
+  const pageTitle = pages.reduce((acc, page) => {
+    if (page.active && page.submenus.length > 0) {
+      const activeSubmenu = page.submenus.find((submenu) => submenu.active);
+      if (activeSubmenu) {
+        return activeSubmenu.label;
+      }
+    } else if (page.active) {
+      return page.label;
+    }
 
-  if (pathname === '/dashboard') {
-    pageTitle = 'Dashboard';
-  } else if (pathname === '/account') {
-    pageTitle = 'Account';
-  }
+    return acc;
+  }, '');
 
   return <h1 className='font-bold'>{pageTitle}</h1>;
 }

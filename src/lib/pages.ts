d8289516ec7b5bfc59db69ac0cbf75a9b30cab73
Mohamad Salimi <contact@salimi.my>
2024-04-25 15:57:19 +0800

    Create pages lib

---
+++ src/lib/pages.ts
@@ -0,0 +1,49 @@
+import { LayoutGrid, Users, SquareCheck, UserCog } from 'lucide-react';
+
+export function getPages(pathname: string) {
+  return [
+    {
+      href: '/dashboard',
+      label: 'Dashboard',
+      active: pathname === '/dashboard',
+      icon: LayoutGrid,
+      menu: true,
+      submenus: []
+    },
+    {
+      href: '',
+      label: 'Tasks',
+      active: pathname.includes('/tasks'),
+      icon: SquareCheck,
+      menu: true,
+      submenus: [
+        {
+          href: '/tasks',
+          label: 'All Tasks',
+          active: pathname === '/tasks'
+        },
+        {
+          href: '/projects/new',
+          label: 'New Task',
+          active: pathname === '/projects/new'
+        }
+      ]
+    },
+    {
+      href: '/users',
+      label: 'Users',
+      active: pathname === '/users',
+      icon: Users,
+      menu: true,
+      submenus: []
+    },
+    {
+      href: '/account',
+      label: 'Account',
+      active: pathname === '/account',
+      icon: UserCog,
+      menu: true,
+      submenus: []
+    }
+  ];
+}

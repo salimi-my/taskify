755680aedb7a63a53483354c17a714f45154fa88
Mohamad Salimi <contact@salimi.my>
2024-04-25 15:58:43 +0800

    Update menu component

--- src/components/protected/menu.tsx
+++ src/components/protected/menu.tsx
@@ -1,12 +1,14 @@
 'use client';
 
 import Link from 'next/link';
+import { LogOut } from 'lucide-react';
 import { signOut } from 'next-auth/react';
 import { usePathname } from 'next/navigation';
-import { User, LogOut, LayoutGrid } from 'lucide-react';
 
 import { cn } from '@/lib/utils';
+import { getPages } from '@/lib/pages';
 import { Button } from '@/components/ui/button';
+import CollapseMenuButton from '@/components/protected/collapse-menu-button';
 import {
   Tooltip,
   TooltipTrigger,
@@ -20,59 +22,56 @@ interface MenuProps {
 
 export function Menu({ isOpen }: MenuProps) {
   const pathname = usePathname();
-
-  const routes = [
-    {
-      href: '/dashboard',
-      label: 'Dashboard',
-      active: pathname === '/dashboard',
-      icon: <LayoutGrid size={18} />
-    },
-    {
-      href: '/account',
-      label: 'Account',
-      active: pathname === '/account',
-      icon: <User size={18} />
-    }
-  ];
+  const pages = getPages(pathname);
 
   return (
     <nav className='mt-8 h-full'>
       <ul className='flex flex-col h-full items-start space-y-1 px-2'>
-        {routes.map((route) => (
-          <li className='w-full' key={route.href}>
-            <TooltipProvider disableHoverableContent>
-              <Tooltip delayDuration={100}>
-                <TooltipTrigger asChild>
-                  <Button
-                    variant={route.active ? 'secondary' : 'ghost'}
-                    className='w-full justify-start h-10'
-                    asChild
-                  >
-                    <Link href={route.href}>
-                      <span className={cn(isOpen === false ? '' : 'mr-4')}>
-                        {route.icon}
-                      </span>
-                      <p
-                        className={cn(
-                          'whitespace-nowrap transition-[transform,opacity] ease-in-out duration-300',
-                          isOpen === false
-                            ? '-translate-x-96 opacity-0'
-                            : 'translate-x-0 opacity-100'
-                        )}
+        {pages
+          .filter((page) => page.menu !== false)
+          .map(({ href, label, icon: Icon, active, submenus }, index) =>
+            submenus.length === 0 ? (
+              <li className='w-full' key={index}>
+                <TooltipProvider disableHoverableContent>
+                  <Tooltip delayDuration={100}>
+                    <TooltipTrigger asChild>
+                      <Button
+                        variant={active ? 'secondary' : 'ghost'}
+                        className='w-full justify-start h-10'
+                        asChild
                       >
-                        {route.label}
-                      </p>
-                    </Link>
-                  </Button>
-                </TooltipTrigger>
-                {isOpen === false && (
-                  <TooltipContent side='right'>{route.label}</TooltipContent>
-                )}
-              </Tooltip>
-            </TooltipProvider>
-          </li>
-        ))}
+                        <Link href={href}>
+                          <span className={cn(isOpen === false ? '' : 'mr-4')}>
+                            <Icon size={18} />
+                          </span>
+                          <p
+                            className={cn(
+                              'whitespace-nowrap',
+                              isOpen === false ? 'opacity-0' : 'opacity-100'
+                            )}
+                          >
+                            {label}
+                          </p>
+                        </Link>
+                      </Button>
+                    </TooltipTrigger>
+                    {isOpen === false && (
+                      <TooltipContent side='right'>{label}</TooltipContent>
+                    )}
+                  </Tooltip>
+                </TooltipProvider>
+              </li>
+            ) : (
+              <CollapseMenuButton
+                key={index}
+                icon={Icon}
+                label={label}
+                active={active}
+                submenus={submenus}
+                isOpen={isOpen}
+              />
+            )
+          )}
         <li className='w-full grow flex items-end'>
           <TooltipProvider disableHoverableContent>
             <Tooltip delayDuration={100}>
@@ -87,10 +86,8 @@ export function Menu({ isOpen }: MenuProps) {
                   </span>
                   <p
                     className={cn(
-                      'whitespace-nowrap transition-[transform,opacity] ease-in-out duration-300',
-                      isOpen === false
-                        ? '-translate-x-96 opacity-0 hidden'
-                        : 'translate-x-0 opacity-100'
+                      'whitespace-nowrap',
+                      isOpen === false ? 'opacity-0 hidden' : 'opacity-100'
                     )}
                   >
                     Sign out

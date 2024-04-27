a8d4845b0fc576c78207b61c8f682fa13ed9d855
Mohamad Salimi <contact@salimi.my>
2024-04-28 02:35:13 +0800

    Create format date util

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,18 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { twMerge } from 'tailwind-merge';
+import { type ClassValue, clsx } from 'clsx';
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function formatDate(
+  date: Date | string | number,
+  opts: Intl.DateTimeFormatOptions = {}
+) {
+  return new Intl.DateTimeFormat('en-GB', {
+    day: opts.day ?? 'numeric',
+    month: opts.month ?? 'long',
+    year: opts.year ?? 'numeric',
+    ...opts
+  }).format(new Date(date));
 }

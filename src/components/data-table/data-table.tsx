308b973e82b28924295cba536c48b9fecffb4e1f
Mohamad Salimi <contact@salimi.my>
2024-04-28 19:27:51 +0800

    Create data-table component

---
+++ src/components/data-table/data-table.tsx
@@ -0,0 +1,80 @@
+import * as React from 'react';
+import { flexRender, type Table as TanstackTable } from '@tanstack/react-table';
+
+import { DataTablePagination } from '@/components/data-table/data-table-pagination';
+import {
+  Table,
+  TableRow,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader
+} from '@/components/ui/table';
+
+interface DataTableProps<TData> {
+  /**
+   * The table instance returned from useDataTable hook with pagination, sorting, filtering, etc.
+   * @type TanstackTable<TData>
+   */
+  table: TanstackTable<TData>;
+}
+
+export function DataTable<TData>({ table }: DataTableProps<TData>) {
+  return (
+    <div className='w-full space-y-2.5 overflow-auto'>
+      <div className='rounded-md border'>
+        <Table>
+          <TableHeader>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map((header) => {
+                  return (
+                    <TableHead key={header.id}>
+                      {header.isPlaceholder
+                        ? null
+                        : flexRender(
+                            header.column.columnDef.header,
+                            header.getContext()
+                          )}
+                    </TableHead>
+                  );
+                })}
+              </TableRow>
+            ))}
+          </TableHeader>
+          <TableBody>
+            {table.getRowModel().rows?.length ? (
+              table.getRowModel().rows.map((row) => (
+                <TableRow
+                  key={row.id}
+                  data-state={row.getIsSelected() && 'selected'}
+                >
+                  {row.getVisibleCells().map((cell) => (
+                    <TableCell key={cell.id}>
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext()
+                      )}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell
+                  colSpan={table.getAllColumns().length}
+                  className='h-24 text-center'
+                >
+                  No results.
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </div>
+      <div className='flex flex-col gap-2.5'>
+        <DataTablePagination table={table} />
+      </div>
+    </div>
+  );
+}

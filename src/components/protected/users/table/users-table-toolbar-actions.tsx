68e70f295d7abcaa3f2edf8bbbbc00c396fe187d
Mohamad Salimi <contact@salimi.my>
2024-04-29 01:07:59 +0800

    Add delete user toolbar action

--- src/components/protected/users/table/users-table-toolbar-actions.tsx
+++ src/components/protected/users/table/users-table-toolbar-actions.tsx
@@ -1,5 +1,6 @@
 import type { User } from '@prisma/client';
 import type { Table } from '@tanstack/react-table';
+import DeleteUsersDialog from './delete-users-dialog';
 
 interface UsersTableToolbarActionsProps {
   table: Table<User>;
@@ -9,6 +10,13 @@ export default function UsersTableToolbarActions({
   table
 }: UsersTableToolbarActionsProps) {
   return (
-    <div className='flex items-center gap-2'>UsersTableToolbarActions</div>
+    <div className='flex items-center gap-2'>
+      {table.getFilteredSelectedRowModel().rows.length > 0 ? (
+        <DeleteUsersDialog
+          users={table.getFilteredSelectedRowModel().rows}
+          onSuccess={() => table.toggleAllPageRowsSelected(false)}
+        />
+      ) : null}
+    </div>
   );
 }

f82b577f85f9a98ddd674a20cf15e0dbe0f1ea0a
Mohamad Salimi <contact@salimi.my>
2024-04-29 14:15:40 +0800

    Update delete user dialog

--- src/components/protected/users/table/delete-users-dialog.tsx
+++ src/components/protected/users/table/delete-users-dialog.tsx
@@ -1,12 +1,11 @@
 'use client';
 
 import { toast } from 'sonner';
+import { useTransition } from 'react';
 import { Loader2 } from 'lucide-react';
 import type { User } from '@prisma/client';
 import { useRouter } from 'next/navigation';
-import { useState, useTransition } from 'react';
 import { type Row } from '@tanstack/react-table';
-import { TrashIcon } from '@radix-ui/react-icons';
 
 import { Button } from '@/components/ui/button';
 import { deleteUser } from '@/actions/users/delete-user';
@@ -22,20 +21,27 @@ import {
 
 interface DeleteUsersDialogProps
   extends React.ComponentPropsWithoutRef<typeof Dialog> {
+  isOpen: boolean;
+  onClose: () => void;
   users: Row<User>[];
   onSuccess?: () => void;
-  showTrigger?: boolean;
 }
 
 export default function DeleteUsersDialog({
   users,
+  isOpen,
+  onClose,
   onSuccess,
-  showTrigger = true,
   ...props
 }: DeleteUsersDialogProps) {
   const router = useRouter();
-  const [isOpen, setIsOpen] = useState(false);
   const [isPending, startTransition] = useTransition();
+
+  const onChange = (open: boolean) => {
+    if (!open) {
+      onClose();
+    }
+  };
 
   const onDelete = () => {
     startTransition(() => {
@@ -46,7 +52,7 @@ export default function DeleteUsersDialog({
           }
 
           if (data.success) {
-            setIsOpen(false);
+            onClose();
             toast.success(data.success);
             router.refresh();
           }
@@ -56,46 +62,38 @@ export default function DeleteUsersDialog({
   };
 
   return (
-    <>
-      {showTrigger ? (
-        <Button variant='outline' size='sm' onClick={() => setIsOpen(true)}>
-          <TrashIcon className='mr-2 size-4' aria-hidden='true' />
-          Delete ({users.length})
-        </Button>
-      ) : null}
-      <Dialog open={isOpen} onOpenChange={setIsOpen} {...props}>
-        <DialogContent>
-          <DialogHeader>
-            <DialogTitle>Are you sure?</DialogTitle>
-            <DialogDescription>
-              This action cannot be undone. This will permanently delete{' '}
-              <span className='font-medium'>{users.length}</span>
-              {users.length === 1 ? ' user' : ' users'} from database.
-            </DialogDescription>
-          </DialogHeader>
-          <DialogFooter className='gap-2 sm:space-x-0'>
-            <DialogClose asChild>
-              <Button variant='outline' disabled={isPending}>
-                Cancel
-              </Button>
-            </DialogClose>
-            <Button
-              aria-label='Delete selected rows'
-              variant='destructive'
-              onClick={onDelete}
-              disabled={isPending}
-            >
-              {isPending && (
-                <>
-                  <Loader2 className='animate-spin mr-2' size={18} />
-                  Deleting...
-                </>
-              )}
-              {!isPending && <>Delete</>}
+    <Dialog open={isOpen} onOpenChange={onChange} {...props}>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Are you sure?</DialogTitle>
+          <DialogDescription>
+            This action cannot be undone. This will permanently delete{' '}
+            <span className='font-medium'>{users.length}</span>
+            {users.length === 1 ? ' user' : ' users'} from database.
+          </DialogDescription>
+        </DialogHeader>
+        <DialogFooter className='gap-2 sm:space-x-0'>
+          <DialogClose asChild>
+            <Button variant='outline' disabled={isPending}>
+              Cancel
             </Button>
-          </DialogFooter>
-        </DialogContent>
-      </Dialog>
-    </>
+          </DialogClose>
+          <Button
+            aria-label='Delete selected rows'
+            variant='destructive'
+            onClick={onDelete}
+            disabled={isPending}
+          >
+            {isPending && (
+              <>
+                <Loader2 className='animate-spin mr-2' size={18} />
+                Deleting...
+              </>
+            )}
+            {!isPending && <>Delete</>}
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
   );
 }
--- src/components/protected/users/table/users-table-toolbar-actions.tsx
+++ src/components/protected/users/table/users-table-toolbar-actions.tsx
@@ -1,6 +1,12 @@
+'use client';
+
+import { useState } from 'react';
+
 import type { User } from '@prisma/client';
+import { Button } from '@/components/ui/button';
+import { TrashIcon } from '@radix-ui/react-icons';
 import type { Table } from '@tanstack/react-table';
-import DeleteUsersDialog from './delete-users-dialog';
+import DeleteUsersDialog from '@/components/protected/users/table/delete-users-dialog';
 
 interface UsersTableToolbarActionsProps {
   table: Table<User>;
@@ -9,13 +15,27 @@ interface UsersTableToolbarActionsProps {
 export default function UsersTableToolbarActions({
   table
 }: UsersTableToolbarActionsProps) {
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+
   return (
     <div className='flex items-center gap-2'>
       {table.getFilteredSelectedRowModel().rows.length > 0 ? (
-        <DeleteUsersDialog
-          users={table.getFilteredSelectedRowModel().rows}
-          onSuccess={() => table.toggleAllPageRowsSelected(false)}
-        />
+        <>
+          <Button
+            variant='outline'
+            size='sm'
+            onClick={() => setIsDialogOpen(true)}
+          >
+            <TrashIcon className='mr-2 size-4' aria-hidden='true' />
+            Delete ({table.getFilteredSelectedRowModel().rows.length})
+          </Button>
+          <DeleteUsersDialog
+            isOpen={isDialogOpen}
+            onClose={() => setIsDialogOpen(false)}
+            users={table.getFilteredSelectedRowModel().rows}
+            onSuccess={() => table.toggleAllPageRowsSelected(false)}
+          />
+        </>
       ) : null}
     </div>
   );

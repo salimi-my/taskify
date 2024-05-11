03ca50d30343543e68a9ea0a579d3f7e36fd4910
Mohamad Salimi <contact@salimi.my>
2024-05-11 15:49:49 +0800

    Wrap content layout inside a div

--- src/components/admin-panel/content-layout.tsx
+++ src/components/admin-panel/content-layout.tsx
@@ -7,9 +7,9 @@ interface ContentLayoutProps {
 
 export function ContentLayout({ title, children }: ContentLayoutProps) {
   return (
-    <>
+    <div>
       <Navbar title={title} />
       <div className='container pt-8 pb-8 px-4 sm:px-8'>{children}</div>
-    </>
+    </div>
   );
 }

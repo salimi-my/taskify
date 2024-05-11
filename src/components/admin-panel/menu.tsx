26f19158a990f115578f27d107c85dad2c7a1101
Mohamad Salimi <contact@salimi.my>
2024-05-11 22:46:23 +0800

    Update signout button onclick

--- src/components/admin-panel/menu.tsx
+++ src/components/admin-panel/menu.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import Link from 'next/link';
+import { signOut } from 'next-auth/react';
 import { usePathname } from 'next/navigation';
 import { Ellipsis, LogOut } from 'lucide-react';
 
@@ -108,7 +109,7 @@ export function Menu({ isOpen }: MenuProps) {
               <Tooltip delayDuration={100}>
                 <TooltipTrigger asChild>
                   <Button
-                    onClick={() => {}}
+                    onClick={() => signOut()}
                     variant='outline'
                     className='w-full justify-center h-10 mt-4'
                   >

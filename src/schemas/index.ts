afadb5e1b42481eaa46b1a2e6b8f2257c313465f
Mohamad Salimi <contact@salimi.my>
2024-05-13 22:44:06 +0800

    Add create project schema

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -122,3 +122,8 @@ export const ProjectFilterSchema = z.object({
   from: z.string().optional(),
   to: z.string().optional()
 });
+
+export const CreateProjectSchema = z.object({
+  name: z.string().min(1, { message: 'Name is required.' }),
+  description: z.string().min(1, { message: 'Description is required.' })
+});

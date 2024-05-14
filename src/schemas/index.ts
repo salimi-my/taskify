1e1beb3554e8b9ff8871e3ee5c65223647f1691e
Mohamad Salimi <contact@salimi.my>
2024-05-15 01:03:14 +0800

    Add edit project schema

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -127,3 +127,8 @@ export const CreateProjectSchema = z.object({
   name: z.string().min(1, { message: 'Name is required.' }),
   description: z.string().min(1, { message: 'Description is required.' })
 });
+
+export const EditProjectSchema = z.object({
+  name: z.string().min(1, { message: 'Name is required.' }),
+  description: z.string().min(1, { message: 'Description is required.' })
+});

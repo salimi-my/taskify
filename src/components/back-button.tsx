6dc7f6ad0da9c567713a1236d8ee6d316b92cda5
Mohamad Salimi <contact@salimi.my>
2024-05-19 01:33:22 +0800

    style: change back button size & styling

--- src/components/back-button.tsx
+++ src/components/back-button.tsx
@@ -1,9 +1,9 @@
 'use client';
 
+import { ArrowLeft } from 'lucide-react';
 import { useRouter } from 'next/navigation';
 
 import { Button } from '@/components/ui/button';
-import { ArrowLeft } from 'lucide-react';
 
 interface BackButtonProps {
   slug?: string;
@@ -16,9 +16,8 @@ export function BackButton({ slug, label }: BackButtonProps) {
   return (
     <Button
       onClick={() => (slug ? router.push(slug) : router.back())}
-      className='bg-white dark:text-primary-foreground dark:hover:text-primary dark:hover:border-slate-500 mb-4'
+      className='mb-4'
       variant='outline'
-      size='sm'
     >
       <ArrowLeft className='size-4 mr-1' />
       {label || 'Back'}

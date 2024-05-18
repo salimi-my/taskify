4225b9f0d8f2e54abe9115544da4cb06c8cbab6e
Mohamad Salimi <contact@salimi.my>
2024-05-19 01:22:52 +0800

    feat: add create task page

---
+++ src/app/(protected)/tasks/create/page.tsx
@@ -0,0 +1,41 @@
+import { ContentLayout } from '@/components/admin-panel/content-layout';
+import {
+  Card,
+  CardTitle,
+  CardHeader,
+  CardContent,
+  CardDescription
+} from '@/components/ui/card';
+
+export default function CreateTaskPage() {
+  return (
+    <ContentLayout title='New Task'>
+      <div className='grid xl:grid-cols-8 gap-4'>
+        <div className='xl:col-span-5'>
+          <Card className='rounded-lg border-none'>
+            <CardHeader className='mx-[1px] pb-9'>
+              <CardTitle className='text-xl font-semibold'>
+                Create Task
+              </CardTitle>
+              <CardDescription>
+                Fill in the form to create a new task.
+              </CardDescription>
+            </CardHeader>
+            <CardContent>{/* TODO: Add create task form */}</CardContent>
+          </Card>
+        </div>
+        <div className='xl:col-span-3'>
+          <Card className='rounded-lg border-none'>
+            <CardHeader className='mx-[1px] pb-9'>
+              <CardTitle className='text-xl font-semibold'>
+                Task Details
+              </CardTitle>
+              <CardDescription>Manage additional task details.</CardDescription>
+            </CardHeader>
+            <CardContent>{/* TODO: Add task details form */}</CardContent>
+          </Card>
+        </div>
+      </div>
+    </ContentLayout>
+  );
+}

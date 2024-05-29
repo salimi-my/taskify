9b560feeb7b6f2db7f4041296dc2ca1cf6168f7a
Mohamad Salimi <contact@salimi.my>
2024-05-29 22:29:14 +0800

    style: update multiple selector focus ring & min height

--- src/components/ui/multiple-selector.tsx
+++ src/components/ui/multiple-selector.tsx
@@ -390,7 +390,7 @@ const MultipleSelector = React.forwardRef<
       >
         <div
           className={cn(
-            'min-h-10 rounded-md border border-input text-sm ring-offset-background focus-within:ring-1 focus-within:ring-ring',
+            'min-h-9 rounded-md border border-input text-sm focus-within:outline-none ring-inset focus-within:ring-1 focus-within:ring-ring',
             {
               'px-3 py-2': selected.length !== 0,
               'cursor-text': !disabled && selected.length !== 0

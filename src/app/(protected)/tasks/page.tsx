9a50b9cd1a589ac8195706712c46771a80c831ca
Mohamad Salimi <contact@salimi.my>
2024-06-01 21:49:22 +0800

    feat: update tasks page layout

--- src/app/(protected)/tasks/page.tsx
+++ src/app/(protected)/tasks/page.tsx
@@ -1,6 +1,10 @@
+import { Suspense } from 'react';
+
 import { SearchParams } from '@/types';
 import { TaskFilterSchema } from '@/schemas';
+import { DataTableCard } from '@/components/protected/data-table-card';
 import { ContentLayout } from '@/components/admin-panel/content-layout';
+import { DataTableSkeleton } from '@/components/data-table/data-table-skeleton';
 
 export interface TasksPageProps {
   searchParams: SearchParams;
@@ -9,5 +13,24 @@ export interface TasksPageProps {
 export default async function TasksPage({ searchParams }: TasksPageProps) {
   const search = TaskFilterSchema.parse(searchParams);
 
-  return <ContentLayout title='All Tasks'>TasksPage</ContentLayout>;
+  return (
+    <ContentLayout title='All Tasks'>
+      <DataTableCard title='Tasks' description="Here's the list of all tasks.">
+        <Suspense
+          key={JSON.stringify(search)}
+          fallback={
+            <DataTableSkeleton
+              columnCount={6}
+              searchableColumnCount={1}
+              filterableColumnCount={2}
+              cellWidths={['15rem', '25rem', '10rem', '10rem', '8rem', '8rem']}
+              shrinkZero
+            />
+          }
+        >
+          TasksPage
+        </Suspense>
+      </DataTableCard>
+    </ContentLayout>
+  );
 }

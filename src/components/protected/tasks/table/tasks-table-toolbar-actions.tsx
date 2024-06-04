5bd05541e969519609d00766e762ab9b2767a222
Mohamad Salimi <contact@salimi.my>
2024-06-04 23:17:00 +0800

    feat: create tasks table toolbar actions component

---
+++ src/components/protected/tasks/table/tasks-table-toolbar-actions.tsx
@@ -0,0 +1,51 @@
+'use client';
+
+import { useState } from 'react';
+import { type Task } from '@prisma/client';
+import type { Table } from '@tanstack/react-table';
+import { PlusIcon, TrashIcon } from '@radix-ui/react-icons';
+
+import { Button } from '@/components/ui/button';
+
+type TaskWithProject = Task & {
+  project: string;
+};
+
+interface TasksTableToolbarActionsProps {
+  table: Table<TaskWithProject>;
+}
+
+export function TasksTableToolbarActions({
+  table
+}: TasksTableToolbarActionsProps) {
+  const [isDeleteOpen, setIsDeleteOpen] = useState(false);
+
+  return (
+    <div className='flex items-center gap-2'>
+      {table.getFilteredSelectedRowModel().rows.length > 0 ? (
+        <>
+          <Button
+            variant='outline'
+            size='sm'
+            onClick={() => setIsDeleteOpen(true)}
+            className='group'
+          >
+            <TrashIcon
+              className='mr-2 size-4 group-hover:text-destructive'
+              aria-hidden='true'
+            />
+            <span className='group-hover:text-destructive'>
+              Delete ({table.getFilteredSelectedRowModel().rows.length})
+            </span>
+          </Button>
+        </>
+      ) : null}
+      <>
+        <Button variant='outline' size='sm' onClick={() => {}}>
+          <PlusIcon className='mr-2 size-4' aria-hidden='true' />
+          New task
+        </Button>
+      </>
+    </div>
+  );
+}
